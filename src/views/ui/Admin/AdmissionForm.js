cc33d6766a0c1a3907198883473117c71f941141
Shashwat <shashwatmittal.india@gmail.com>
2022-11-24 18:40:14 +0530

    added content to admission form, change password, Student Academic (upload marks) in admin

--- src/views/ui/Admin/AdmissionForm.js
+++ src/views/ui/Admin/AdmissionForm.js
@@ -1,208 +1,123 @@
-import React, { useState } from "react";
+//Forms
 import {
-  Alert,
-  UncontrolledAlert,
   Card,
-  CardBody,
+  Row,
+  Col,
   CardTitle,
+  CardBody,
+  Button,
+  Form,
+  FormGroup,
+  Label,
+  Input,
+  FormText,
 } from "reactstrap";
 
-const Alerts = () => {
-  // For Dismiss Button with Alert
-  const [visible, setVisible] = useState(true);
-
-  const onDismiss = () => {
-    setVisible(false);
-  };
-
+const Forms = () => {
   return (
-    <div>
-      {/* --------------------------------------------------------------------------------*/}
-      {/* Card-1*/}
-      {/* --------------------------------------------------------------------------------*/}
-      <Card>
-        <CardTitle tag="h6" className="border-bottom p-3 mb-0">
-          <i className="bi bi-bell me-2"> </i>
-          Alert
-        </CardTitle>
-        <CardBody className="">
-          <div className="mt-3">
-            <Alert color="primary">
-              This is a primary alert— check it out!
-            </Alert>
-            <Alert color="secondary">
-              This is a secondary alert— check it out!
-            </Alert>
-            <Alert color="success">
-              This is a success alert— check it out!
-            </Alert>
-            <Alert color="danger">This is a danger alert— check it out!</Alert>
-            <Alert color="warning">
-              This is a warning alert— check it out!
-            </Alert>
-            <Alert color="info">This is a info alert— check it out!</Alert>
-            <Alert color="light">This is a light alert— check it out!</Alert>
-            <Alert color="dark">This is a dark alert</Alert>
-          </div>
-        </CardBody>
-      </Card>
-      {/* --------------------------------------------------------------------------------*/}
-      {/* Card-2*/}
-      {/* --------------------------------------------------------------------------------*/}
-      <Card>
-        <CardTitle tag="h6" className="border-bottom p-3 mb-0">
-          <i className="bi bi-bell me-2" />
-          Alert with Links
-        </CardTitle>
-        <CardBody className="">
-          <div>
-            <Alert color="primary">
-              This is a primary alert with
-              <a href="/" className="alert-link">
-                an example link
-              </a>
-              . Give it a click if you like.
-            </Alert>
-            <Alert color="secondary">
-              This is a secondary alert with
-              <a href="/" className="alert-link">
-                an example link
-              </a>
-              . Give it a click if you like.
-            </Alert>
-            <Alert color="success">
-              This is a success alert with
-              <a href="/" className="alert-link">
-                an example link
-              </a>
-              . Give it a click if you like.
-            </Alert>
-            <Alert color="danger">
-              This is a danger alert with
-              <a href="/" className="alert-link">
-                an example link
-              </a>
-              . Give it a click if you like.
-            </Alert>
-            <Alert color="warning">
-              This is a warning alert with
-              <a href="/" className="alert-link">
-                an example link
-              </a>
-              . Give it a click if you like.
-            </Alert>
-            <Alert color="info">
-              This is a info alert with
-              <a href="/" className="alert-link">
-                an example link
-              </a>
-              . Give it a click if you like.
-            </Alert>
-            <Alert color="light">
-              This is a light alert with
-              <a href="/" className="alert-link">
-                an example link
-              </a>
-              . Give it a click if you like.
-            </Alert>
-            <Alert color="dark">
-              This is a dark alert with
-              <a href="/" className="alert-link">
-                an example link
-              </a>
-              . Give it a click if you like.
-            </Alert>
-          </div>
-        </CardBody>
-      </Card>
-      {/* --------------------------------------------------------------------------------*/}
-      {/* Card-3*/}
-      {/* --------------------------------------------------------------------------------*/}
-      <Card>
-        <CardTitle tag="h6" className="border-bottom p-3 mb-0">
-          <i className="bi bi-bell me-2" />
-          Alert with Additional content
-        </CardTitle>
-        <CardBody className="">
-          <div>
-            <Alert color="success">
-              <h4 className="alert-heading">Well done!</h4>
-              <p>
-                Aww yeah, you successfully read this important alert message.
-                This example text is going to run a bit longer so that you can
-                see how spacing within an alert works with this kind of content.
-              </p>
-              <hr />
-              <p className="mb-0">
-                Whenever you need to, be sure to use margin utilities to keep
-                things nice and tidy.
-              </p>
-            </Alert>
-          </div>
-        </CardBody>
-      </Card>
-      {/* --------------------------------------------------------------------------------*/}
-      {/* Card-4*/}
-      {/* --------------------------------------------------------------------------------*/}
-      <Card>
-        <CardTitle tag="h6" className="border-bottom p-3 mb-0">
-          <i className="bi bi-bell me-2" />
-          Alert with Dissmissing
-        </CardTitle>
-        <CardBody className="">
-          <div>
-            <Alert color="info" isOpen={visible} toggle={onDismiss.bind(null)}>
-              I am an alert and I can be dismissed!
-            </Alert>
-          </div>
-        </CardBody>
-      </Card>
-      {/* --------------------------------------------------------------------------------*/}
-      {/* Card-5*/}
-      {/* --------------------------------------------------------------------------------*/}
-      <Card>
-        <CardTitle tag="h6" className="border-bottom p-3 mb-0">
-          <i className="bi bi-bell me-2" />
-          Alert with Uncontrolled [disable] Alerts
-        </CardTitle>
-        <CardBody className="">
-          <div>
-            <UncontrolledAlert color="info">
-              I am an alert and I can be dismissed!
-            </UncontrolledAlert>
-          </div>
-        </CardBody>
-      </Card>
-      {/* --------------------------------------------------------------------------------*/}
-      {/* Card-6*/}
-      {/* --------------------------------------------------------------------------------*/}
-      <Card>
-        <CardTitle tag="h6" className="border-bottom p-3 mb-0">
-          <i className="bi bi-bell me-2" />
-          Alerts without fade
-        </CardTitle>
-        <CardBody className="">
-          <div>
-            <Alert
-              color="primary"
-              isOpen={visible}
-              toggle={onDismiss.bind(null)}
-              fade={false}
-            >
-              I am a primary alert and I can be dismissed without animating!
-            </Alert>
-            <UncontrolledAlert color="warning" fade={false}>
-              I am an alert and I can be dismissed without animating!
-            </UncontrolledAlert>
-          </div>
-        </CardBody>
-      </Card>
+    <Row>
+      <Col>
+        {/* --------------------------------------------------------------------------------*/}
+        {/* Card-1*/}
+        {/* --------------------------------------------------------------------------------*/}
+        <Card>
+          <CardTitle tag="h6" className="border-bottom p-3 mb-0">
+            <i className="bi bi-pen me-2"> </i>
+            Admission Details
+          </CardTitle>
+          <CardBody>
+            <Form>
 
-      {/* --------------------------------------------------------------------------------*/}
-      {/* End Inner Div*/}
-      {/* --------------------------------------------------------------------------------*/}
-    </div>
+              <FormGroup>
+                <Label for="studentName">Student Name</Label>
+                <Input id="studentName" name="text" type="text" />
+              </FormGroup>
+              <FormGroup>
+                <Label for="admissionNumber">Admission Number</Label>
+                <Input id="admissionNumber" name="text" type="number" />
+              </FormGroup>
+              
+              <FormGroup>
+                <Label for="class">Class</Label>
+                <Input id="class" name="select" type="select">
+                  <option>1</option>
+                  <option>2</option>
+                  <option>3</option>
+                  <option>4</option>
+                  <option>5</option>
+                  <option>6</option>
+                  <option>7</option>
+                  <option>8</option>
+                  <option>9</option>
+                  <option>10</option>
+                  <option>11</option>
+                  <option>12</option>
+                </Input>
+              </FormGroup>
+              <FormGroup>
+                <Label for="dob">Date of Birth</Label>
+                <Input id="dob" name="date" type="date" />
+              </FormGroup>
+              <FormGroup>
+                <Label for="joiningDate">Date of Joining</Label>
+                <Input id="joiningDate" name="date" type="date" />
+              </FormGroup>
+              <FormGroup>
+                <Label for="fatherName">Father's Name</Label>
+                <Input id="fatherName" name="text" type="text" />
+              </FormGroup>
+              <FormGroup>
+                <Label for="motherName">Mother's Name</Label>
+                <Input id="motherName" name="text" type="text" />
+              </FormGroup>
+              <FormGroup>
+                <Label for="gender">Gender</Label>
+                <Input id="gender" name="select" type="select">
+                  <option>Male</option>
+                  <option>Female</option>
+                  <option>Other</option>
+                </Input>
+              </FormGroup>
+              <FormGroup>
+                <Label for="category">Category</Label>
+                <Input id="category" name="text" type="text" />
+              </FormGroup>
+              <FormGroup>
+                <Label for="nationality">Nationality</Label>
+                <Input id="nationality" name="text" type="text" />
+              </FormGroup>
+              <FormGroup>
+                <Label for="address">Address</Label>
+                <Input id="address" name="text" type="textarea" />
+              </FormGroup>
+              <FormGroup>
+                <Label for="studentImage">Image</Label>
+                <Input id="studentImage" name="file" type="file" />
+              </FormGroup>
+              <FormGroup>
+                <Label for="exampleEmail">Email</Label>
+                <Input
+                  id="exampleEmail"
+                  name="email"
+                  type="email"
+                />
+              </FormGroup>
+              <FormGroup>
+                <Label for="examplePassword">Password</Label>
+                <Input
+                  id="examplePassword"
+                  name="password"
+                  type="password"
+                />
+              </FormGroup>
+
+              <Button>Submit</Button>
+            </Form>
+          </CardBody>
+        </Card>
+      </Col>
+    </Row>
   );
 };
 
-export default Alerts;
+export default Forms;
--- src/views/ui/Admin/ChangePassword.js
+++ src/views/ui/Admin/ChangePassword.js
@@ -1,131 +1,62 @@
-import { Container, Col, Row, Card, CardBody, CardTitle } from "reactstrap";
+//Forms
+import {
+  Card,
+  Row,
+  Col,
+  CardTitle,
+  CardBody,
+  Button,
+  Form,
+  FormGroup,
+  Label,
+  Input,
+  FormText,
+} from "reactstrap";
 
-const Grid = () => {
+const Forms = () => {
   return (
-    <div>
-      {/* --------------------------------------------------------------------------------*/}
-      {/* Start Inner Div*/}
-      {/* --------------------------------------------------------------------------------*/}
-      {/* --------------------------------------------------------------------------------*/}
-      {/* Row*/}
-      {/* --------------------------------------------------------------------------------*/}
-      <Card>
-        <CardTitle tag="h6" className="border-bottom p-3 mb-0">
-          Grid Layout
-        </CardTitle>
-        <CardBody className="">
-          <Container>
-            <Row>
-              <Col>
-                <div className="bg-light p-2 border">.col</div>
-              </Col>
-            </Row>
-            <Row className="mt-3">
-              <Col>
-                <div className="bg-light p-2 border">.col</div>
-              </Col>
-              <Col>
-                <div className="bg-light p-2 border">.col</div>
-              </Col>
-              <Col>
-                <div className="bg-light p-2 border">.col</div>
-              </Col>
-              <Col>
-                <div className="bg-light p-2 border">.col</div>
-              </Col>
-            </Row>
-            <Row className="mt-3">
-              <Col xs="3">
-                <div className="bg-light p-2 border">.col-3</div>
-              </Col>
-              <Col xs="auto">
-                <div className="bg-light p-2 border">
-                  .col-auto - variable width content
-                </div>
-              </Col>
-              <Col xs="3">
-                <div className="bg-light p-2 border">.col-3</div>
-              </Col>
-            </Row>
-            <Row className="mt-3">
-              <Col xs="6">
-                <div className="bg-light p-2 border">.col-6</div>
-              </Col>
-              <Col xs="6">
-                <div className="bg-light p-2 border">.col-6</div>
-              </Col>
-            </Row>
-            <Row className="mt-3">
-              <Col xs="6" sm="4">
-                <div className="bg-light p-2 border">.col-6 .col-sm-4</div>
-              </Col>
-              <Col xs="6" sm="4">
-                <div className="bg-light p-2 border">.col-6 .col-sm-4</div>
-              </Col>
-              <Col sm="4">
-                <div className="bg-light p-2 border">.col-sm-4</div>
-              </Col>
-            </Row>
-            <Row className="mt-3">
-              <Col
-                sm={{
-                  offset: 1,
-                  order: 2,
-                  size: 6,
-                }}
-              >
-                <div className="bg-light p-2 border">
-                  .col-sm-6 .col-sm-order-2 .col-sm-offset-2
-                </div>
-              </Col>
-            </Row>
-            <Row className="mt-3">
-              <Col
-                sm="12"
-                md={{
-                  offset: 2,
-                  size: 8,
-                }}
-              >
-                <div className="bg-light p-2 border">
-                  .col-sm-12 .col-md-6 .col-md-offset-3
-                </div>
-              </Col>
-            </Row>
-            <Row className="mt-3">
-              <Col
-                sm={{
-                  offset: 1,
-                  size: "auto",
-                }}
-              >
-                <div className="bg-light p-2 border">
-                  .col-sm .col-sm-offset-1
-                </div>
-              </Col>
-              <Col
-                sm={{
-                  offset: 1,
-                  size: "auto",
-                }}
-              >
-                <div className="bg-light p-2 border">
-                  .col-sm .col-sm-offset-1
-                </div>
-              </Col>
-            </Row>
-          </Container>
-        </CardBody>
-      </Card>
-      {/* --------------------------------------------------------------------------------*/}
-      {/* Row*/}
-      {/* --------------------------------------------------------------------------------*/}
+    <Row>
+      <Col>
+        {/* --------------------------------------------------------------------------------*/}
+        {/* Card-1*/}
+        {/* --------------------------------------------------------------------------------*/}
+        <Card>
+          <CardTitle tag="h6" className="border-bottom p-3 mb-0">
+            <i className="bi bi-key me-2"> </i>
+            Change Student Password
+          </CardTitle>
+          <CardBody>
+            <Form>
 
-      {/* --------------------------------------------------------------------------------*/}
-      {/* End Inner Div*/}
-      {/* --------------------------------------------------------------------------------*/}
-    </div>
+              <FormGroup>
+                <Label for="rollNo">Student Roll Number</Label>
+                <Input id="rollNo" name="rollno" type="number" />
+              </FormGroup>             
+              
+              <FormGroup>
+                <Label for="exampleEmail">Student Email</Label>
+                <Input
+                  id="exampleEmail"
+                  name="email"
+                  type="email"
+                />
+              </FormGroup>
+              <FormGroup>
+                <Label for="examplePassword">New Password</Label>
+                <Input
+                  id="examplePassword"
+                  name="password"
+                  type="password"
+                />
+              </FormGroup>
+
+              <Button>Submit</Button>
+            </Form>
+          </CardBody>
+        </Card>
+      </Col>
+    </Row>
   );
 };
 
-export default Grid;
+export default Forms;
