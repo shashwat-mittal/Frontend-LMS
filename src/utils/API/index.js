bfce229a1675ff2f8d74028c114e76ef3abce9fc
BuddyLongLegs <=>
2022-11-27 23:37:20 +0530

    created handleSubmit function for AdmissionForm

--- src/utils/API/index.js
+++ src/utils/API/index.js
@@ -4,74 +4,89 @@ import { getLS } from "../LocalStorage/index";
 const API_URL = "http://localhost:8000/api";
 
 const getAccessToken = () => {
-  return getLS("jwt_token");
+    return getLS("jwt_token");
 };
 
 const getHeaders = (token) => {
-  if (!token) token = getAccessToken();
-  if (token){
-    console.log(token);
+    if (!token) token = getAccessToken();
+    if (token) {
+        console.log(token);
+        return {
+            headers: {
+                Accept: "application/json",
+                Authorization: `Bearer ${token}`,
+            },
+        };
+    }
     return {
-      headers: {
-        Accept: "application/json",
-        Authorization: `Bearer ${token}`,
-      },
+        headers: {
+            Accept: "application/json",
+        },
     };
-  }
-  return {
-    headers: {
-      Accept: "application/json",
-    },
-  };
 };
 
 const post = async (endpoint, body, token = null) => {
-  try {
-    const response = await axios.post(
-      API_URL + endpoint,
-      body,
-      getHeaders(token)
-    );
-    return response;
-  } catch (err) {
-    console.error(err?.response?.data || err);
-    return err?.response?.data || err;
-  }
+    try {
+        const response = await axios.post(
+            API_URL + endpoint,
+            body,
+            getHeaders(token)
+        );
+        return response;
+    } catch (err) {
+        console.error(err?.response?.data || err);
+        return err?.response?.data || err;
+    }
+};
+
+const postMultipart = async (endpoint, body, token = null) => {
+    let headers = getHeaders(token);
+    headers["headers"]["CONTENT_TYPE"] = "multipart/form-data";
+    try {
+        const response = await axios.post(API_URL + endpoint, body, headers);
+        return response;
+    } catch (err) {
+        console.error(err?.response?.data || err);
+        return err?.response?.data || err;
+    }
 };
 
 const get = async (endpoint, token = null) => {
-  try {
-    console.log(token);
-    const response = await axios.get(API_URL + endpoint, getHeaders(token));
-    return response;
-  } catch (err) {
-    console.error(err?.response?.data || err);
-    return err?.response?.data || err;
-  }
+    try {
+        console.log(token);
+        const response = await axios.get(API_URL + endpoint, getHeaders(token));
+        return response;
+    } catch (err) {
+        console.error(err?.response?.data || err);
+        return err?.response?.data || err;
+    }
 };
 
 const put = async (endpoint, body, token = null) => {
-  try {
-    const response = await axios.put(
-      API_URL + endpoint,
-      body,
-      getHeaders(token)
-    );
-    return response.data;
-  } catch (err) {
-    console.error(err?.response?.data || err);
-    return err?.response?.data || err;
-  }
+    try {
+        const response = await axios.put(
+            API_URL + endpoint,
+            body,
+            getHeaders(token)
+        );
+        return response.data;
+    } catch (err) {
+        console.error(err?.response?.data || err);
+        return err?.response?.data || err;
+    }
 };
 
 const remove = async (endpoint, token = null) => {
-  try {
-    const response = await axios.delete(API_URL + endpoint, getHeaders(token));
-    return response.data;
-  } catch (err) {
-    console.error(err?.response?.data || err);
-    return err?.response?.data || err;
-  }
+    try {
+        const response = await axios.delete(
+            API_URL + endpoint,
+            getHeaders(token)
+        );
+        return response.data;
+    } catch (err) {
+        console.error(err?.response?.data || err);
+        return err?.response?.data || err;
+    }
 };
 
-export { getAccessToken, post, get, put, remove };
+export { getAccessToken, post, get, put, remove, postMultipart };
--- src/views/ui/Admin/AdmissionForm.js
+++ src/views/ui/Admin/AdmissionForm.js
@@ -1,123 +1,166 @@
 //Forms
 import {
-  Card,
-  Row,
-  Col,
-  CardTitle,
-  CardBody,
-  Button,
-  Form,
-  FormGroup,
-  Label,
-  Input,
-  FormText,
+    Card,
+    Row,
+    Col,
+    CardTitle,
+    CardBody,
+    Button,
+    Form,
+    FormGroup,
+    Label,
+    Input,
+    FormText,
 } from "reactstrap";
 
+import { useRef } from "react";
+import { postMultipart } from "../../../utils/API";
+
 const Forms = () => {
-  return (
-    <Row>
-      <Col>
-        {/* --------------------------------------------------------------------------------*/}
-        {/* Card-1*/}
-        {/* --------------------------------------------------------------------------------*/}
-        <Card>
-          <CardTitle tag="h6" className="border-bottom p-3 mb-0">
-            <i className="bi bi-pen me-2"> </i>
-            Admission Details
-          </CardTitle>
-          <CardBody>
-            <Form>
+    const form = useRef(null);
+    async function handleSubmit(e) {
+        e.preventDefault();
+        let formData = new FormData(form.current);
+        let data = await postMultipart("/api/students/students", formData);
+        console.log(data);
+    }
+    return (
+        <Row>
+            <Col>
+                {/* --------------------------------------------------------------------------------*/}
+                {/* Card-1*/}
+                {/* --------------------------------------------------------------------------------*/}
+                <Card>
+                    <CardTitle tag="h6" className="border-bottom p-3 mb-0">
+                        <i className="bi bi-pen me-2"> </i>
+                        Admission Details
+                    </CardTitle>
+                    <CardBody>
+                        <Form ref={form} onSubmit={handleSubmit}>
+                            <FormGroup>
+                                <Label for="studentName">Student Name</Label>
+                                <Input
+                                    id="studentName"
+                                    name="name"
+                                    type="text"
+                                />
+                            </FormGroup>
+                            <FormGroup>
+                                <Label for="admissionNumber">
+                                    Admission Number
+                                </Label>
+                                <Input
+                                    id="admissionNumber"
+                                    name="enrollment_id"
+                                    type="number"
+                                />
+                            </FormGroup>
 
-              <FormGroup>
-                <Label for="studentName">Student Name</Label>
-                <Input id="studentName" name="text" type="text" />
-              </FormGroup>
-              <FormGroup>
-                <Label for="admissionNumber">Admission Number</Label>
-                <Input id="admissionNumber" name="text" type="number" />
-              </FormGroup>
-              
-              <FormGroup>
-                <Label for="class">Class</Label>
-                <Input id="class" name="select" type="select">
-                  <option>1</option>
-                  <option>2</option>
-                  <option>3</option>
-                  <option>4</option>
-                  <option>5</option>
-                  <option>6</option>
-                  <option>7</option>
-                  <option>8</option>
-                  <option>9</option>
-                  <option>10</option>
-                  <option>11</option>
-                  <option>12</option>
-                </Input>
-              </FormGroup>
-              <FormGroup>
-                <Label for="dob">Date of Birth</Label>
-                <Input id="dob" name="date" type="date" />
-              </FormGroup>
-              <FormGroup>
-                <Label for="joiningDate">Date of Joining</Label>
-                <Input id="joiningDate" name="date" type="date" />
-              </FormGroup>
-              <FormGroup>
-                <Label for="fatherName">Father's Name</Label>
-                <Input id="fatherName" name="text" type="text" />
-              </FormGroup>
-              <FormGroup>
-                <Label for="motherName">Mother's Name</Label>
-                <Input id="motherName" name="text" type="text" />
-              </FormGroup>
-              <FormGroup>
-                <Label for="gender">Gender</Label>
-                <Input id="gender" name="select" type="select">
-                  <option>Male</option>
-                  <option>Female</option>
-                  <option>Other</option>
-                </Input>
-              </FormGroup>
-              <FormGroup>
-                <Label for="category">Category</Label>
-                <Input id="category" name="text" type="text" />
-              </FormGroup>
-              <FormGroup>
-                <Label for="nationality">Nationality</Label>
-                <Input id="nationality" name="text" type="text" />
-              </FormGroup>
-              <FormGroup>
-                <Label for="address">Address</Label>
-                <Input id="address" name="text" type="textarea" />
-              </FormGroup>
-              <FormGroup>
-                <Label for="studentImage">Image</Label>
-                <Input id="studentImage" name="file" type="file" />
-              </FormGroup>
-              <FormGroup>
-                <Label for="exampleEmail">Email</Label>
-                <Input
-                  id="exampleEmail"
-                  name="email"
-                  type="email"
-                />
-              </FormGroup>
-              <FormGroup>
-                <Label for="examplePassword">Password</Label>
-                <Input
-                  id="examplePassword"
-                  name="password"
-                  type="password"
-                />
-              </FormGroup>
+                            <FormGroup>
+                                <Label for="class">Class</Label>
+                                <Input id="class" name="select" type="select">
+                                    <option>1</option>
+                                    <option>2</option>
+                                    <option>3</option>
+                                    <option>4</option>
+                                    <option>5</option>
+                                    <option>6</option>
+                                    <option>7</option>
+                                    <option>8</option>
+                                    <option>9</option>
+                                    <option>10</option>
+                                    <option>11</option>
+                                    <option>12</option>
+                                </Input>
+                            </FormGroup>
+                            <FormGroup>
+                                <Label for="dob">Date of Birth</Label>
+                                <Input id="dob" name="dob" type="date" />
+                            </FormGroup>
+                            {/* <FormGroup>
+                                <Label for="joiningDate">Date of Joining</Label>
+                                <Input
+                                    id="joiningDate"
+                                    name="date"
+                                    type="date"
+                                />
+                            </FormGroup> */}
+                            <FormGroup>
+                                <Label for="fatherName">Father's Name</Label>
+                                <Input
+                                    id="fatherName"
+                                    name="father"
+                                    type="text"
+                                />
+                            </FormGroup>
+                            <FormGroup>
+                                <Label for="motherName">Mother's Name</Label>
+                                <Input
+                                    id="motherName"
+                                    name="mother"
+                                    type="text"
+                                />
+                            </FormGroup>
+                            <FormGroup>
+                                <Label for="gender">Gender</Label>
+                                <Input id="gender" name="select" type="select">
+                                    <option>Male</option>
+                                    <option>Female</option>
+                                    <option>Other</option>
+                                </Input>
+                            </FormGroup>
+                            {/* <FormGroup>
+                                <Label for="category">Category</Label>
+                                <Input id="category" name="text" type="text" />
+                            </FormGroup> */}
+                            <FormGroup>
+                                <Label for="nationality">Nationality</Label>
+                                <Input
+                                    id="nationality"
+                                    name="text"
+                                    type="text"
+                                />
+                            </FormGroup>
+                            <FormGroup>
+                                <Label for="address">Address</Label>
+                                <Input
+                                    id="address"
+                                    name="text"
+                                    type="textarea"
+                                />
+                            </FormGroup>
+                            <FormGroup>
+                                <Label for="studentImage">Image</Label>
+                                <Input
+                                    id="studentImage"
+                                    name="file"
+                                    type="file"
+                                />
+                            </FormGroup>
+                            <FormGroup>
+                                <Label for="exampleEmail">Email</Label>
+                                <Input
+                                    id="exampleEmail"
+                                    name="email"
+                                    type="email"
+                                />
+                            </FormGroup>
+                            <FormGroup>
+                                <Label for="examplePassword">Password</Label>
+                                <Input
+                                    id="examplePassword"
+                                    name="password"
+                                    type="password"
+                                />
+                            </FormGroup>
 
-              <Button>Submit</Button>
-            </Form>
-          </CardBody>
-        </Card>
-      </Col>
-    </Row>
-  );
+                            <Button type="submit">Submit</Button>
+                        </Form>
+                    </CardBody>
+                </Card>
+            </Col>
+        </Row>
+    );
 };
 
 export default Forms;
