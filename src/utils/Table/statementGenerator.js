fdb0f581571fb830ac427ae7c1129703726b87e7
Aditya Verma <adityarubbers.av@gmail.com>
2022-11-27 23:28:15 +0530

    feat: Added List Student page

---
+++ src/utils/Table/statementGenerator.js
@@ -0,0 +1,47 @@
+import jsPDF from "jspdf";
+import "jspdf-autotable";
+// Date Fns is used to format the dates we receive
+// from our API call
+import { format } from "date-fns";
+
+const generatePDF = (statements) => {
+  const doc = new jsPDF();
+
+  const tableColumn = [
+    "ID",
+    "Type of Transaction",
+    "Amount",
+    "Date",
+    "Receiver account ID",
+    "Sender account ID",
+  ];
+
+  const tableRows = [];
+  console.log(statements);
+  statements.forEach((object) => {
+    if (object.s_account == null) {
+      object.s_account = "Deposited by admin";
+    }
+    const objectData = [
+      object.transaction_id,
+      object.type,
+      object.amount,
+      format(new Date(object.timestamp), "yyyy-MM-dd"),
+      object.r_account,
+      object.s_account,
+    ];
+
+    tableRows.push(objectData);
+  });
+  console.log(tableRows);
+
+  doc.autoTable(tableColumn, tableRows, { startY: 20 });
+  const date = Date().split(" ");
+
+  const dateStr = date[0] + date[1] + date[2] + date[3] + date[4];
+
+  doc.text("Account Statement", 14, 15);
+  doc.save(`statement_${dateStr}.pdf`);
+};
+
+export default generatePDF;
--- src/views/ui/Admin/StudentforAdmin.js
+++ src/views/ui/Admin/StudentforAdmin.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import {
   Card,
   Row,
@@ -16,7 +16,9 @@ import { get, post } from "../../../utils/API";
 const Forms = () => {
   const [change, setChange] = useState("");
   const [classes, setClasses] = useState([]);
+  const [selectedClass, setSelectedClass] = useState();
   const [formSubmitted, setFormSubmitted] = useState(false);
+  const [classData, setClassData] = useState([]);
   const getClassData = async (e) => {
     const response = await get("/academics/class");
     console.log(response);
@@ -26,12 +28,20 @@ const Forms = () => {
     setChange("change");
   };
   const handleSubmitForm = async (e) => {
-    const response = await get(`/students/students?classSection`);
+    const response = await get(
+      `/students/students?class_section=${selectedClass}`
+    );
+    if (response.status === 200) {
+      console.log(response.data);
+      setClassData(response.data);
+    }
+    setFormSubmitted(true);
   };
   function handleClassChange(e) {
-      console.log(e.target);
+    setSelectedClass(e.target.value);
+    console.log(e.target.value);
   }
-
+  
   useEffect(() => {
     getClassData();
   }, [change]);
@@ -51,10 +61,16 @@ const Forms = () => {
               <Form>
                 <FormGroup>
                   <Label for="class">Class</Label>
-                  <Input id="class" name="select" type="select">
+                  <Input
+                    id="class"
+                    name="select"
+                    type="select"
+                    value={selectedClass}
+                    onChange={handleClassChange}
+                  >
                     {classes.map((item) => {
                       return (
-                        <option key={item.id}>
+                        <option key={item.id} value={item.id}>
                           {item.classname + " " + item.section}
                         </option>
                       );
@@ -71,37 +87,78 @@ const Forms = () => {
     );
   } else {
     return (
-      <Row>
-        <Col>
-          {/* --------------------------------------------------------------------------------*/}
-          {/* Card-1*/}
-          {/* --------------------------------------------------------------------------------*/}
-          <Card>
-            <CardTitle tag="h6" className="border-bottom p-3 mb-0">
-              <i className="bi bi-book me-2"> </i>
-              Students List
-            </CardTitle>
-            <CardBody>
-              <Form>
-                <FormGroup>
-                  <Label for="class">Class</Label>
-                  <Input id="class" name="select" type="select" onChange={handleClassChange}>
-                    {classes.map((item) => {
-                      return (
-                        <option key={item.id}>
-                          {item.classname + " " + item.section}
-                        </option>
-                      );
-                    })}
-                  </Input>
-                </FormGroup>
+      <div>
+        <Row>
+          <Col>
+            {/* --------------------------------------------------------------------------------*/}
+            {/* Card-1*/}
+            {/* --------------------------------------------------------------------------------*/}
+            <Card>
+              <CardTitle tag="h6" className="border-bottom p-3 mb-0">
+                <i className="bi bi-book me-2"> </i>
+                Students List
+              </CardTitle>
+              <CardBody>
+                <Form>
+                  <FormGroup>
+                    <Label for="class">Class</Label>
+                    <Input
+                      id="class"
+                      name="select"
+                      type="select"
+                      value={selectedClass}
+                      onChange={handleClassChange}
+                    >
+                      {classes.map((item) => {
+                        return (
+                          <option key={item.id} value={item.id}>
+                            {item.classname + " " + item.section}
+                          </option>
+                        );
+                      })}
+                    </Input>
+                  </FormGroup>
 
-                <Button onClick={handleSubmitForm}>Submit</Button>
-              </Form>
-            </CardBody>
-          </Card>
-        </Col>
-      </Row>
+                  <Button onClick={handleSubmitForm}>Submit</Button>
+                </Form>
+              </CardBody>
+            </Card>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <table class="table  table-hover">
+              <thead>
+                <tr className="table-dark">
+                  <th scope="col">User ID</th>
+                  <th scope="col">Name</th>
+                  <th scope="col">Address</th>
+                  <th scope="col">Date of Birth</th>
+                  <th scope="col">Phone No.</th>
+                  <th scope="col">Enrollment No.</th>
+                  <th scope="col">Class Section</th>
+                </tr>
+              </thead>
+              <tbody>
+                {classData.map((val, key) => {
+                  return (
+                    <tr key={key}>
+                      <th scope="row">{val.id}</th>
+                      <td>{val.name}</td>
+                      <td>{val.address}</td>
+                      <td>{val.dob}</td>
+                      <td>{val.phone_number}</td>
+                      <td>{val.enrollment_id}</td>
+                      <td>{val.class_section}</td>
+
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+          </Col>
+        </Row>
+      </div>
     );
   }
 };
